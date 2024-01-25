ed462f11d8f286b70f7ff13e861ff6b1ee276173
Palenhame <111377258+Palenhame@users.noreply.github.com>
2024-01-25 18:44:25 +0300

    create model

--- test_django/main/models.py
+++ test_django/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Test(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Name')
+    description = models.TextField(verbose_name='Description')
+
+    class Meta:
+        verbose_name = 'Test'
+
