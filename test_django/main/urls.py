b2336e511af965573cb8bb399c09a19bb08b86cd
Palenhame <111377258+Palenhame@users.noreply.github.com>
2024-01-26 17:56:17 +0300

    try to make news pages

--- test_django/main/urls.py
+++ test_django/main/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import main
+from .views import main, other
 
 app_name = 'main'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name='main'),
+    path('<int:page>', other, name='other')
 ]
--- test_django/main/views.py
+++ test_django/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Test
 from .form import TestForm
 
 
@@ -6,10 +7,18 @@ from .form import TestForm
 
 def main(request):
     if request.method == 'POST':
+        data = Test.objects.values_list('pk')
         form = TestForm(request.POST)
         if form.is_valid():
             form.save()
 
     else:
         form = TestForm()
-    return render(request, 'index.html', {'form': form})
+        data = None
+    return render(request, 'index.html',
+                  {'form': form, 'data': data})
+
+
+def other(request, page):
+    data = Test.objects.get(pk=page)
+    return render(request, 'other.html', {'data': data})
