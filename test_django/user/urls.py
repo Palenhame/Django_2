353808db56c81889d38308badbe5c6e788df8003
Palenhame <111377258+Palenhame@users.noreply.github.com>
2024-01-29 17:45:12 +0300

    save before start doing users

---
+++ test_django/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import register
+
+
+app_name = 'user'
+urlpatterns = [
+    # path('login/', None, name='login'),
+    # path('logout/', None, name='logout'),
+    path('register/', register, name='register'),
+]
