204c1b64981b98c6dc6c030dec785ef39d3459ec
Palenhame <111377258+Palenhame@users.noreply.github.com>
2024-02-11 15:10:53 +0300

    continue doing register, logout, login

--- test_django/user/urls.py
+++ test_django/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, LoginUser, user_logout
+from .views import register, LoginUser, user_logout, account
 
 
 app_name = 'user'
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', user_logout, name='logout'),
     path('register/', register, name='register'),
+    path('test/', account, name='test'),
 
 
 ]
