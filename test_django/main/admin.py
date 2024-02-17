36ed57426dba6fa2caf85e886e8d089f5cdaa539
Palenhame <111377258+Palenhame@users.noreply.github.com>
2024-02-17 20:25:54 +0300

    start doing comments

--- test_django/main/admin.py
+++ test_django/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import NewsModel
+from .models import NewsModel, CommentModel
 
 # Register your models here.
 admin.site.register(NewsModel)
+admin.site.register(CommentModel)
